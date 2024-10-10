7f850b3a642c982dc13cac23d4eaffe51154d04d
Tim_Smith <timothy.smithii@tamu.edu>
2024-10-09 23:12:22 -0500

    Basic Queries

--- answers.sh
+++ answers.sh
@@ -2,9 +2,9 @@ SELECT Name FROM Students;
 SELECT * FROM Students WHERE Age > 30;
 SELECT Name FROM Students WHERE Gender='F' AND Age=30;
 SELECT Points FROM Students WHERE Name='Alex';
-INSERT INTO Students VALUES(10, 'Nabigha', '24', 'F', 500);
-UPDATE Students SET Points = 500 WHERE Name='Basma';
-UPDATE Students SET Points = 100 WHERE Name='Alex';
+INSERT INTO Students VALUES(10, 'Tim', '24', 'M', 250);
+UPDATE students SET Points = Points + 3 WHERE Name = 'Basma';
+UPDATE Students SET Points = Points - 5 WHERE Name='Alex';
 
 CREATE TABLE graduates(
     ID INTEGER  PRIMARY KEY AUTOINCREMENT,
