72ec00d4eb2d31476d8f855dd73779e13abcdc6f
Tim_Smith <timothy.smithii@tamu.edu>
2024-10-09 23:23:15 -0500

    Creating Table

--- answers.sh
+++ answers.sh
@@ -15,9 +15,9 @@ CREATE TABLE graduates(
 	Graduation TEXT
 );
 
-INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
+INSERT INTO graduates(ID, Name, Age, Gender, Points) SELECT ID, Name, Age, Gender, Points FROM Students WHERE Students.Name='Layal';
 UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
-DELETE FROM graduates WHERE Name='Layal';
+DELETE FROM students WHERE Name='Layal';
 
 SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
 SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
